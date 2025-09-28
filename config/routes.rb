d3c843794431994f225120f272e6e1adc4b42ffa
JohnReyGimenez <gimenezjr5@gmail.com>
2025-09-28 22:00:14 +0800

    added function show in controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :todos
 
   get "/projects", to: "projects#index"
+  get "/projects:id", to: "project#show", as: "project"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
